6703ed4daa9776fcbf7c5337f32f0a72a4b9d3a9
5nail000 <105508594+5nail000@users.noreply.github.com>
2022-09-30 10:23:56 +0300

    reDesign of variables

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import argparse
 def create_bitlink(url, token):
 
     headers = {'Authorization': f'Bearer {token}'}
-    url = {'long_url': url}
+    long_url = {'long_url': url}
     response = requests.post(
         f'https://api-ssl.bitly.com/v4/bitlinks',
         headers=headers,
-        json=url
+        json=long_url
     )
     response.raise_for_status()
     return response.json()['id']
@@ -21,6 +21,7 @@ def create_bitlink(url, token):
 def count_clicks(link, token):
 
     headers = {'Authorization': f'Bearer {token}'}
+    link = f'{urlparse(link).netloc}{urlparse(link).path}'
     response = requests.get(
         f'https://api-ssl.bitly.com/v4/bitlinks/{link}/clicks/summary',
         headers=headers
@@ -32,6 +33,7 @@ def count_clicks(link, token):
 def is_bitlink(link, token):
 
     headers = {'Authorization': f'Bearer {token}'}
+    link = f'{urlparse(link).netloc}{urlparse(link).path}'
     response = requests.get(
         f'https://api-ssl.bitly.com/v4/bitlinks/{link}',
         headers=headers
@@ -48,11 +50,11 @@ def main():
     parser.add_argument("user_link", help="link for bitly service")
 
     args = parser.parse_args()
-    link = f'{urlparse(args.user_link).netloc}{urlparse(args.user_link).path}'
+    
 
-    if is_bitlink(link, token):
+    if is_bitlink(args.user_link, token):
         print(
-            f'Количество переходов по ссылке битли: {(count_clicks(link, token))}')
+            f'Количество переходов по ссылке битли: {(count_clicks(args.user_link, token))}')
     else:
         print(create_bitlink(args.user_link, token))
 
